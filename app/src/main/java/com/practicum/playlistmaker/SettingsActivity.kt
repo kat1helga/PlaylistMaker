49f19c31f79cec75ebfe7cd75e6f8f282136d236
ekaterina.reshetnikova <ekaterina.reshetnikova@dif.tech>
2024-04-23 00:50:00 +0300

    add realization to buttons on settings page

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,46 @@ class SettingsActivity : AppCompatActivity() {
         buttonBackToMain.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+        }
+
+        val buttonShare = findViewById<ImageView>(R.id.share_button)
+
+        buttonShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            val message = "https://practicum.yandex.ru/android-developer/"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            val chooserIntent = Intent.createChooser(shareIntent, "Поделиться приложением")
+            startActivity(chooserIntent)
+        }
+
+        val buttonGroup = findViewById<ImageView>(R.id.group_button)
+        val supportEmail = "адрес_почты_студента"
+        buttonGroup.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(
+                    Intent.EXTRA_EMAIL,
+                    arrayOf(supportEmail),
+                )
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    "Сообщение разработчикам и разработчицам приложения Playlist Maker",
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "Спасибо разработчикам и разработчицам за крутое приложение!",
+                )
+            }
+            startActivity(intent)
+        }
+
+        val buttonConditions = findViewById<ImageView>(R.id.conditions_button)
+
+        buttonConditions.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
         }
     }
 }
